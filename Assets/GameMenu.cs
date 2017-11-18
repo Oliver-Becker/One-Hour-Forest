d4e66e7e5172abe922c3321f0af413316df02d3e
Oliver <oliverbecker@usp.br>
2017-11-18 20:09:50 -0200

    menus!

---
+++ Assets/GameMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+	public GameObject Menu;
+
+	public void open_menu() {
+		Menu.SetActive (true);
+	}
+
+	public void close_menu() {
+		Menu.SetActive (false);
+	}
+
+	public void return_main_menu() {
+		SceneManager.LoadScene ("Menu");
+	}
+
+}
---
+++ Assets/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public GameObject MainScreen;
+	public GameObject CreditScreen;
+
+	public void goto_main() {
+		MainScreen.SetActive (true);
+		CreditScreen.SetActive (false);
+	}
+
+	public void goto_credits() {
+		MainScreen.SetActive (false);
+		CreditScreen.SetActive (true);
+	}
+
+	public void play() {
+		SceneManager.LoadScene ("Game");
+	}
+
+}
